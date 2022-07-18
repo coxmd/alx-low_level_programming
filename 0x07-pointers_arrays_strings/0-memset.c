4de6a6fb84e261d27591dfa9591961f7d2e276f3
coxMD <coxmusyoki@gmail.com>
2022-07-18 05:53:10 -0700

    added file 1

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,6 +5,8 @@
  * @s: memory area to be filled
  * @b: char to copy
  * @n: number of times to copy b
+ * Description: Fill the first `n` bytes of the memory area pointed to by `s`
+ * with the constant byte `b`.
  * Return: pointer to the memory area s
  */
 char *_memset(char *s, char b, unsigned int n)
